9aa017b15df6c992a4d9dba22312e8b2b35d629f
Keith R <keithrmathman@gmail.com>
2022-12-14 22:34:31 -0600

    added aws ses code

--- App.js
+++ App.js
@@ -630,6 +630,59 @@ modalButtonOpen: {
     let {width, height} = event.nativeEvent.layout
     this.setState({headerDimensions: {width, height}})
   }
+
+  sendEmail(email, messageText){
+    // Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'REGION'});
+
+// Create sendEmail params 
+var params = {
+  Destination: { /* required */
+    CcAddresses: [
+      /* more items */
+    ],
+    ToAddresses: [
+      'sandyrobinson0@yahoo.com',
+      /* more items */
+    ]
+  },
+  Message: { /* required */
+    Body: { /* required */
+      Html: {
+       Charset: "UTF-8",
+       Data: "HTML_FORMAT_BODY"
+      },
+      Text: {
+       Charset: "UTF-8",
+       Data: "TEXT_FORMAT_BODY"
+      }
+     },
+     Subject: {
+      Charset: 'UTF-8',
+      Data: 'Message From: ' + email
+     }
+    },
+  Source: 'SENDER_EMAIL_ADDRESS', /* required */
+  ReplyToAddresses: [
+     'EMAIL_ADDRESS',
+    /* more items */
+  ],
+};
+
+// Create the promise and SES service object
+var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+
+// Handle promise's fulfilled/rejected states
+sendPromise.then(
+  function(data) {
+    console.log(data.MessageId);
+  }).catch(
+    function(err) {
+    console.error(err, err.stack);
+  });
+  }
  render () {
   const { slider1ActiveSlide } = this.state;
   const images= [
